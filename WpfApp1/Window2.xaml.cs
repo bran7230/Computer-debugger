cd67291afba86d43e9793020b6fa6531432a8259
Brandon Trumble <brandon.ta.trumble@icloud.com>
2025-03-12 02:28:32 -0400

    Added download manager back, and made it list files and when you click the button tells you where it is.

--- WpfApp1/Window2.xaml.cs
+++ WpfApp1/Window2.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +20,29 @@ namespace WpfApp1
     /// </summary>
     public partial class Window2 : Window
     {
-        //nothing here yet
+        DirectoryInfo dir;
+        FileInfo[] files;
+        string userName = Environment.UserName;
         public Window2()
         {
             InitializeComponent();
+            dir = new DirectoryInfo(@"C:\Users\" + userName + "\\Downloads");
+            files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                listBox.Items.Add(file.Name);
+            }
+
         }
+
+       
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show(dir.FullName + "\\" + listBox.SelectedItem.ToString());
+          
+        }
+
+     
     }
 }
